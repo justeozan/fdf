0a2d2f5fc2d4e69570ac058cbee6e058a1965d09
justeozan <justeoz.an@gmail.com>
2024-03-25 16:46:11 +0100

    trying to fix key_manager fram issues

--- src/key_manager.c
+++ src/key_manager.c
@@ -6,7 +6,7 @@
 /*   By: ozasahin <ozasahin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 11:07:58 by ozasahin          #+#    #+#             */
-/*   Updated: 2024/03/25 15:09:14 by ozasahin         ###   ########.fr       */
+/*   Updated: 2024/03/25 16:43:59 by ozasahin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,37 @@ void    do_key(int key, t_matrix **matrix)
 		FDF.scale -= 3;
 }
 
+void	draw_black(t_matrix **matrix)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < FDF.height)
+	{
+		x = 0;
+		while (x < FDF.width)
+		{
+			put_pixel(FDF.imgs, x, y, 0);
+			x++;
+		}
+		y++;
+	}
+}
+
+
 int    manage_key(int key, t_matrix **matrix)
 {
 	ft_printf("key = %d\n", key);
 	if (is_key(key))
 	{
+		draw_black(matrix);
+		mlx_put_image_to_window(FDF.mlx, FDF.win, FDF.imgs.img, 0, 0);
+		// if (mlx_put_image_to_window(FDF.mlx, FDF.win, FDF.imgs.img, 0, 0) < 0)
+		// 	close_program(matrix, "Error\n");
 		do_key(key, matrix);
 		// print_menu(FDF);
-		// frame(matrix);
+		frame(matrix);
 	}
 	// if (key == ONE)
 	//     new_window();
