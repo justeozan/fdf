1cf7c67d603566bd944fcbab1fd0d788a36f4a56
justeozan <justeoz.an@gmail.com>
2024-03-25 17:02:43 +0100

    add auto key_hook

--- src/key_manager.c
+++ src/key_manager.c
@@ -6,7 +6,7 @@
 /*   By: ozasahin <ozasahin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 11:07:58 by ozasahin          #+#    #+#             */
-/*   Updated: 2024/03/25 16:43:59 by ozasahin         ###   ########.fr       */
+/*   Updated: 2024/03/25 16:58:07 by ozasahin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 int is_key(int key)
 {
-	return(key == PLUS || key == MINUS);
+	return(key == UP || key == DOWN || key == LEFT || key == RIGHT
+	|| key == SPACE || key == MINUS || key == PLUS || key == STAR || key == DIV
+	|| key == UP_Z || key == DOWN_Z);
 }
 
 void    do_key(int key, t_matrix **matrix)
@@ -23,6 +25,29 @@ void    do_key(int key, t_matrix **matrix)
 		FDF.scale += 3;
 	if (key == MINUS)
 		FDF.scale -= 3;
+	if (key == UP_Z)
+		FDF.depth += 1;
+	if (key == DOWN_Z)
+		FDF.depth -= 1;
+	if (key == UP)
+		FDF.offset_y -= 10;
+	if (key == DOWN)
+		FDF.offset_y += 10;
+	if (key == LEFT)
+		FDF.offset_x -= 10;
+	if (key == RIGHT)
+		FDF.offset_x += 10;
+	if (key == STAR)
+		FDF.angle += 0.05;
+	if (key == DIV)
+		FDF.angle -= 0.05;
+	if (key == SPACE)
+	{
+		if (FDF.is_isometric == 1)
+			FDF.is_isometric = 0;
+		else
+			FDF.is_isometric = 1;
+	}
 }
 
 void	draw_black(t_matrix **matrix)
@@ -31,10 +56,10 @@ void	draw_black(t_matrix **matrix)
 	int	y;
 
 	y = 0;
-	while (y < FDF.height)
+	while (y < HEIGHT)
 	{
 		x = 0;
-		while (x < FDF.width)
+		while (x < WIDTH)
 		{
 			put_pixel(FDF.imgs, x, y, 0);
 			x++;
@@ -44,15 +69,14 @@ void	draw_black(t_matrix **matrix)
 }
 
 
-int    manage_key(int key, t_matrix **matrix)
+int	manage_key(int key, t_matrix **matrix)
 {
 	ft_printf("key = %d\n", key);
 	if (is_key(key))
 	{
 		draw_black(matrix);
-		mlx_put_image_to_window(FDF.mlx, FDF.win, FDF.imgs.img, 0, 0);
-		// if (mlx_put_image_to_window(FDF.mlx, FDF.win, FDF.imgs.img, 0, 0) < 0)
-		// 	close_program(matrix, "Error\n");
+		if (mlx_put_image_to_window(FDF.mlx, FDF.win, FDF.imgs.img, 0, 0) < 0)
+			close_program(matrix, "Error\n");
 		do_key(key, matrix);
 		// print_menu(FDF);
 		frame(matrix);
